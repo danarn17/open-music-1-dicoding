5a2a3ed78ca6a5728b7cf5be5cafe73a77b31a7b
danarn17 <mdaenar@gmail.com>
2021-07-01 11:19:17 +0700

    final

---
+++ migrations/1625059375124_create-table-songs.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'varchar(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'text',
+      notNull: true,
+    },
+    year: {
+      type: 'integer',
+      notNull: true,
+    },
+    performer: {
+      type: 'text',
+      notNull: true,
+    },
+    genre: {
+      type: 'text',
+      notNull: true,
+    },
+    duration: {
+      type: 'integer',
+      notNull: true,
+    },
+    inserted_at: {
+      type: 'text',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'text',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
---
+++ src/api/songs/handler.js
@@ -0,0 +1,168 @@
+/* eslint-disable require-jsdoc */
+const ClientError = require('../../exceptions/ClientError');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongsHandler = this.getSongsHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+  }
+
+  async postSongHandler(req, h) {
+    try {
+      this._validator.validateSongPayload(req.payload);
+      const {
+        title = 'untitled',
+        year,
+        performer,
+        genre,
+        duration,
+      } = req.payload;
+
+      const songId = await this._service.addSong({
+        title,
+        year,
+        performer,
+        genre,
+        duration,
+      });
+
+      const response = h.response({
+        status: 'success',
+        message: 'Lagu berhasil ditambahkan',
+        data: {
+          songId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      //   SERVER ERROR
+      const response = h.response({
+        status: 'fail',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async getSongsHandler() {
+    const songs = await this._service.getSongs();
+    return {
+      status: 'success',
+      data: {
+        songs,
+      },
+    };
+  }
+
+  async getSongByIdHandler(req, h) {
+    try {
+      const { id } = req.params;
+      const song = await this._service.getSongById(id);
+      return {
+        status: 'success',
+        data: {
+          song,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+      // Server ERROR!
+      const response = h.response({
+        status: 'fail',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async putSongByIdHandler(req, h) {
+    try {
+      this._validator.validateSongPayload(req.payload);
+      const { id } = req.params;
+
+      await this._service.editSongById(id, req.payload);
+
+      return {
+        status: 'success',
+        message: 'Lagu berhasil diperbarui',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'fail',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async deleteSongByIdHandler(req, h) {
+    try {
+      const { id } = req.params;
+      await this._service.deleteSongById(id);
+
+      return {
+        status: 'success',
+        message: 'Lagu berhasil dihapus',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'fail',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = SongsHandler;
